ad82c9a812c9f15cb8d3adc444d767ad660e5a68
melzreal <melissa.real@hotmail.com>
2019-03-13 22:26:21 +0000

    adding validation for unique movie title and comments model"

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,3 @@
 class Movie < ApplicationRecord
+	validates_uniqueness_of :title
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+ movie = Movie.create(title: "Black Panther", description: "Challa heir to Wakanda needs to kick some super butt", year: 2018)  
+  movie = Movie.create(title: "Isle of Dogs", description: "Cool boy goes on a quest for an even cooler dog", year: 2018)  
