90bcd77cfc7537767673f56aa93b308b71c3266a
melzreal <melissa.real@hotmail.com>
2019-05-28 19:04:19 +0100

    added movies view

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,2 +1,27 @@
 class MoviesController < ApplicationController
+ 
+  def index
+  end
+  
+  def new
+    @movie = Movie.new
+  end
+
+
+ def create
+    @movie = Movie.create(movie_params)
+    redirect_to movie_path(@movie)
+  end
+
+
+  def show
+    @movie = Movie.find(params[:id])
+  end
+
+  private
+
+  def movie_params
+    params.require(:movie).permit(:title, :description, :year)
+  end
+
 end
