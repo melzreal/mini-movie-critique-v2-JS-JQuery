a5a1bcf9cae31badaf0a87299df15af2bc9177e0
melzreal <melissa.real@hotmail.com>
2019-06-25 12:27:12 +0100

    added comment scope method

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,9 @@
 class Comment < ApplicationRecord
-  validates_uniqueness_of :text, scope: :user_id
-  has_many :ratings
+
+	scope :love, -> { where('text LIKE ?', "%love%") }
+
+  	validates_uniqueness_of :text, scope: :user_id
+  	has_many :ratings
 
 	belongs_to :user, class_name: 'User', foreign_key: 'user_id'
 	belongs_to :movie, class_name: 'Movie', foreign_key: 'movie_id'
